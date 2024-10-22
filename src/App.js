766ff73aa19e1485dde10858efa4c2f531e679dd
moisesgianizelli <ork.mgm@gmail.com>
2024-10-22 20:29:20 +0100

    First layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+  const Toggle = () => {
+    setToggle(!toggle);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid bg-secondary min-vh-100">
+      <div className="row">
+        {toggle && (
+          <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+            <Sidebar />
+          </div>
+        )}
+        {toggle && <div className="col-4 col-md-2"></div>}
+        <div className="col">
+          <Home Toggle={Toggle} />
+        </div>
+      </div>
     </div>
   );
 }
